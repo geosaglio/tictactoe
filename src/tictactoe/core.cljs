e93680e03dbf8adb8762b1d44f41f1f2667c2751
Geo Saglio <Geo@emas-MacBook-Pro.local>
2019-06-21 16:28:56 +0800

    win conditions

--- src/tictactoe/core.cljs
+++ src/tictactoe/core.cljs
@@ -9,18 +9,25 @@
 ;; define your app data so that it doesn't get over-written on reload
 
 ; (require '[tictactoe.core :refer [state]])
-(defonce state (r/atom {:board {:haut-gauche nil
-                                :haut-milieu nil
-                                :haut-droite nil
-                                :milieu-gauche nil
-                                :milieu-milieu nil
-                                :milieu-droite nil
-                                :bas-gauche nil
-                                :bas-milieu nil
-                                :bas-droite nil}
-                        :turn :joueur-1}))
+(def initial-state {:board {:haut-gauche nil
+                            :haut-milieu nil
+                            :haut-droite nil
+                            :milieu-gauche nil
+                            :milieu-milieu nil
+                            :milieu-droite nil
+                            :bas-gauche nil
+                            :bas-milieu nil
+                            :bas-droite nil}
+                    :turn :joueur-1})
+
+(defonce state (r/atom initial-state))
 ; Lire : @state
 ; Ecrire : (swap! state assoc :text "Hello Geo")
+
+; (reset! state initial-state))
+
+(def ^:const symbols {:joueur-1 :circle
+                      :joueur-2 :cross})
 
 (defn banner
   [state]
@@ -43,13 +50,27 @@
       (change-turn state :joueur-2)
       (change-turn state :joueur-1))))
 
+(defn victoire
+  [state]
+  (let [board (:board @state)
+        player (:turn @state)]
+    (when (or (= (player symbols) (:haut-gauche board) (:haut-milieu board) (:haut-droite board))
+              (= (player symbols) (:haut-gauche board) (:milieu-milieu board) (:bas-droite board))
+              (= (player symbols) (:haut-gauche board) (:milieu-gauche board) (:bas-gauche board))
+              (= (player symbols) (:milieu-gauche board) (:milieu-milieu board) (:milieu-droite board))
+              (= (player symbols) (:bas-gauche board) (:bas-milieu board) (:bas-droite board))
+              (= (player symbols) (:haut-milieu board) (:milieu-milieu board) (:bas-milieu board))
+              (= (player symbols) (:haut-droite board) (:milieu-droite board) (:bas-droite board))
+              (= (player symbols) (:bas-gauche board) (:milieu-milieu board) (:haut-droite board)))
+      (.alert js/window (str player " a gagne")))))
+
 (defn click-button
   [state button-clicked]
-  (let [symbols {:joueur-1 :circle
-                 :joueur-2 :cross}
-        player (:turn @state)]
+  (let [player (:turn @state)]
     (swap! state update :board assoc button-clicked (player symbols)))
+  (victoire state)
   (toggle-player-turn state))
+
 
 (defn ttt-button
   [state localization css]
@@ -74,20 +95,25 @@
    [:div.ligne3
     [ttt-button state :bas-gauche "button3-1"]
     [ttt-button state :bas-milieu "button3-2"]
-    [ttt-button state :bas-droite "button3-3"]]])
+    [ttt-button state :bas-droite "button3-3"]]
+   
+   [:button {:type "button"
+             :class "restartbutton"
+             :on-click #(reset! state initial-state)}
+    "Restart"]])
 
 (defn my-content
   [state]
-  [:div
-   [banner state]
-   [body state]])
-
+   [:div
+    [banner state]
+    [body state]])
+ 
 (defn my-app
-  []
-  [my-content state])
-(render my-app
-        (.getElementById js/document "app"))
-(add-watch state 
-           :rerender 
-           #(render my-app
+   []
+   [my-content state])
+ (render my-app
+         (.getElementById js/document "app"))
+ (add-watch state 
+            :rerender 
+            #(render my-app
                     (.getElementById js/document "app")))
