4d6c9e534d71e587b98c746336a9a2b9ce765c38
Geo Saglio <Geo@emas-MacBook-Pro.local>
2019-06-26 15:49:24 +0800

    GRID v2

--- src/tictactoe/core.cljs
+++ src/tictactoe/core.cljs
@@ -9,16 +9,9 @@
 ;; define your app data so that it doesn't get over-written on reload
 
 ; (require '[tictactoe.core :refer [state]])
-(def initial-state {:board {:haut-gauche nil
-                            :haut-milieu nil
-                            :haut-droite nil
-                            :milieu-gauche nil
-                            :milieu-milieu nil
-                            :milieu-droite nil
-                            :bas-gauche nil
-                            :bas-milieu nil
-                            :bas-droite nil}
-                    :turn :joueur-1})
+(def initial-state {:board {}
+                    :turn :joueur-1
+                    })
 
 (defonce state (r/atom initial-state))
 ; Lire : @state
@@ -62,45 +55,53 @@
               (= (player symbols) (:haut-milieu board) (:milieu-milieu board) (:bas-milieu board))
               (= (player symbols) (:haut-droite board) (:milieu-droite board) (:bas-droite board))
               (= (player symbols) (:bas-gauche board) (:milieu-milieu board) (:haut-droite board)))
-      (.alert js/window (str player " a gagne")))))
+      (.setTimeout js/window #(.alert js/window (str player " a gagne")) 16)
+      (swap! state assoc :winner player))))
 
 (defn click-button
   [state button-clicked]
-  (let [player (:turn @state)]
-    (swap! state update :board assoc button-clicked (player symbols)))
-  (victoire state)
-  (toggle-player-turn state))
-
+  (if (nil? (:winner @state))
+    (do
+      (let [player (:turn @state)]
+        (swap! state update :board assoc button-clicked (player symbols)))
+      (victoire state)
+      (toggle-player-turn state))))
 
 (defn ttt-button
   [state localization css]
-  [:button {:type "button"
-            :class css
-            :on-click #(click-button state localization)}
-   "Place here"])
+  (cond 
+    (= (get-in @state [:board localization]) :cross) 
+    [:svg {:view-box "0 0 100 100"}
+     [:line {:x1 "10" :y1 "100" :x2 "100" :y2 "10"
+             :stroke "black" :stroke-width "10"}]
+     [:line {:x1 "10" :y1 "10" :x2 "100" :y2 "100"
+             :stroke "black" :stroke-width "10"}]]
+    (= (get-in @state [:board localization]) :circle)
+    [:svg {:view-box "0 0 100 100"} 
+     [:circle {:cx "50" :cy "50" :r "45"
+               :stroke "black" :stroke-width "10" :fill "white"}]]
+    (nil? (get-in @state [:board localization]))
+    [:svg {:view-box "0 0 100 100"}
+     [:rect {:width 100 :height 100 
+             :fill "white" :stroke "black" :stroke-width "10"
+             :on-click #(click-button state localization)}]]))
 
 (defn body
   [state]
-  [:div.ttleslignes
-   [:div.ligne1
-    [ttt-button state :haut-gauche "button1"]
-    [ttt-button state :haut-milieu "button2"]
-    [ttt-button state :haut-droite "button3"]]
-
-   [:div.ligne2
-    [ttt-button state :milieu-gauche "button2-1"]
-    [ttt-button state :milieu-milieu "button2-2"]
-    [ttt-button state :milieu-droite "button2-3"]]
-
-   [:div.ligne3
-    [ttt-button state :bas-gauche "button3-1"]
-    [ttt-button state :bas-milieu "button3-2"]
-    [ttt-button state :bas-droite "button3-3"]]
+  [:div.totale [:div.grid-container
+                [:div.grid-item [ttt-button state :haut-gauche "button1"]]
+                [:div.grid-item [ttt-button state :haut-milieu "button2"]]
+                [:div.grid-item [ttt-button state :haut-droite "button3"]]
+                [:div.grid-item [ttt-button state :milieu-gauche "button2-1"]]
+                [:div.grid-item [ttt-button state :milieu-milieu "button2-2"]]
+                [:div.grid-item [ttt-button state :milieu-droite "button2-3"]]
+                [:div.grid-item [ttt-button state :bas-gauche "button3-1"]]
+                [:div.grid-item [ttt-button state :bas-milieu "button3-2"]]
+                [:div.grid-item [ttt-button state :bas-droite "button3-3"]]]
    
    [:button {:type "button"
              :class "restartbutton"
-             :on-click #(reset! state initial-state)}
-    "Restart"]])
+             :on-click #(reset! state initial-state)}"Restart"]])
 
 (defn my-content
   [state]
